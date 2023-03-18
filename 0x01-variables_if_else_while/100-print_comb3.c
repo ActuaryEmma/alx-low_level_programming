80b95c294650d54a170512327dc72bd1dec797f6
ActuaryEmma <emmanyakio@gmail.com>
2023-03-17 18:21:44 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 48; i < 56; i++)
+	for (i = 48; i <= 56; i++)
 	{
-		for (j = 49; j < 57; j++)
+		for (j = 49; j <= 57; j++)
 		{
-			if (i > j)
+			if (j < i)
 			{
 				putchar(i);
 				putchar(j);
