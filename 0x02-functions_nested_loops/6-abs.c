c1bf795a5b5ab28528c290eb66cbcc6ba16f6692
ActuaryEmma <emmanyakio@gmail.com>
2023-03-21 18:07:43 +0000

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _abs - return abolute value of an integer
+  * @int: input value
+  * Return: return absolute value
+  */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
