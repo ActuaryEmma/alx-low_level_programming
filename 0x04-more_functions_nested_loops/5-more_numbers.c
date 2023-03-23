c870d59d82c010f29270e97cc5e1c7e6cc3b63b7
ActuaryEmma <emmanyakio@gmail.com>
2023-03-23 09:07:07 +0000

    print 0- 14 10-times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * more_numbers - print (0 - 1)4 10 times
+  * Return: void
+  */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
