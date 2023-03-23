5067b1942152ca8d1e6020e795fb62cdf5654e1e
ActuaryEmma <emmanyakio@gmail.com>
2023-03-23 07:47:21 +0000

    checks if number is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _isupper - check if character is uppercase
+  * @c: parameter
+  * Return: 1 if uppercase else return 0
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
+}
