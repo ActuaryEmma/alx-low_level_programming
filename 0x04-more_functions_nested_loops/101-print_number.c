c9a732f1085ae5f32732cfbcbf0d5dc4a3a2da1c
ActuaryEmma <emmanyakio@gmail.com>
2023-03-24 09:45:42 +0000

    prime numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,5 +7,24 @@
   */
 void print_number(int n)
 {
-	_putchar(n);
+	int digits = 0;
+
+	int divisor = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n / divisor > 9)
+	{
+		divisor *= 10;
+	}
+	while (divisor != 0)
+	{
+		digits = n / divisor;
+		_putchar(digits + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
 }
