a5dce02cade777329cfc01d6e7a603cb59e77434
ActuaryEmma <emmanyakio@gmail.com>
2023-03-29 13:45:40 +0000

    lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
   * *string_toupper - change lowercase to uppercase
-  * 
+  * @str: parameter
   * Return: 0
   */
 char *string_toupper(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
