d79eab104f8d1ee95d7da2edc65fb72b7d892030
ActuaryEmma <emmanyakio@gmail.com>
2023-03-29 17:27:30 +0000

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * *leet - encodes a string into 1337
+  * @str: parameter
+  * Return: 0
+  */
+char *leet(char *str)
+{
+	int i;
+	int j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
+		}
+	}
+	return (str);
+}
