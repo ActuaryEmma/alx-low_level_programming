a28dd9feaa4082adcf821a0c21423d2f41da8f82
ActuaryEmma <emmanyakio@gmail.com>
2023-04-04 09:15:30 +0000

    add two diagonals in sqaure matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,6 +15,15 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
+		/* formula to access the elements */
+		/**
+		  * sum1 += *(a + (0 * 3) + 0) = 0 element
+		  * sum1 += *(a + (1 * 3) + 1) = 4th element
+		  * sum1 += *(a + (2 * 3) + 2) = 8th element
+		  * sum2 += *(a + (0 * 3) + (3-0-1)) = 2nd element
+		  * sum2 += *(a + (1 * 3) + (3-1-1)) = 4th element
+		  * sum2 += *(a + (2 * 3) + (3-2-1) = 6th element
+		  */
 		sum1 += *(a + i * size + i);
 		sum2 += *(a + i * size + (size - i - 1));
 	}
