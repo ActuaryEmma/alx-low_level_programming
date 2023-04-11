4ad347100a39ca181abfde04ea15c7e3f0e860d5
ActuaryEmma <emmanyakio@gmail.com>
2023-04-11 11:44:06 +0000

    create array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,20 +14,26 @@ char *create_array(unsigned int size, char c)
 
 	char *p;
 
+	/* allocates memory for the array */
 	p = malloc(size * sizeof(char));
 
+	/* first checks the size of array o avoid creating empty array */
 	if (size == 0)
 	{
 		return (NULL);
 	}
+	/* else checks if memory allocation failed and returns NULL if failed */
 	if (p == NULL)
 	{
 		return (NULL);
 	}
+	/* if allocation was successful, the function intializes the array\
+	   with character c . c is assigned to each element in the array */
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
 	}
+	/* returns a pointer to the array */
 	return (p);
 }
 
