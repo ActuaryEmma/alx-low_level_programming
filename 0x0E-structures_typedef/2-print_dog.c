b57330c81ea4ace22686e3f826e4368fba330bd8
ActuaryEmma <emmanyakio@gmail.com>
2023-04-17 14:36:22 +0300

    prints elements of struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
-  * print_dog -  prints the variables
-  * @d: pointer pointing to struct dog
-  * Return: void
-  */
+ * print_dog - prints the variables
+ * @d: pointer pointing to struct dog
+ * Return: void
+ */
 void print_dog(struct dog *d)
 {
 /* check if pointer d is pointing to null i.e not a valid memory address */
@@ -18,17 +18,26 @@ void print_dog(struct dog *d)
 	/* check if elements are null to return nil else otherwise */
 	if (d->name == NULL)
 	{
-		d->name = "(nil)";
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
 	}
 	if (d->age == 0)
 	{
-		d->age = 0;
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
 	}
 	if (d->owner == NULL)
 	{
-		d->owner = "(nil)";
+		printf("Owner: (nil)\n");
 	}
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
