c6cad4bb4338eb1b90131f5e3450cfaa727e438c
ActuaryEmma <emmanyakio@gmail.com>
2023-04-17 22:34:45 +0300

    print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,6 @@ void print_dog(struct dog *d)
 /* check if pointer d is pointing to null i.e not a valid memory address */
 	if (d == NULL)
 	{
-		printf("NULL");
 		return;
 	}
 	/* check if elements are null to return nil else otherwise */
@@ -20,6 +19,7 @@ void print_dog(struct dog *d)
 	{
 		d->name = "(nil)";
 	}
+	
 	printf("Name: %s\n", d->name);
 	/**
 	  *if (d->age == 0)
