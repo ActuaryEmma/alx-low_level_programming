96844754ce83fb10e19dcd1f4cbaa097c3ec14e1
ActuaryEmma <emmanyakio@gmail.com>
2023-04-18 01:51:13 +0300

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -43,7 +43,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (j = 0; j < count2; j++)
 		dog_new->owner[j] = owner[i];
 
+	dog_new->name = name;
 	dog_new->age = age;
+	dog_new->owner = owner;
 	return (dog_new);
 }
 
