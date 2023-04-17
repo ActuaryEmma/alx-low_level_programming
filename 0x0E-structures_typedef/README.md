cda9196388a54e1c78efacdb1a31c69201469661
ActuaryEmma <emmanyakio@gmail.com>
2023-04-17 11:50:59 +0300

    add new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,9 @@
+structures and typeof
+
+Task 0
+Define a new type struct dog with the following elements:
+
+    name, type = char *
+    age, type = float
+    owner, type = char *
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_STRUCT_H
+#define DOG_STRUCT_H
+/**
+  * struct dog- short description
+  * @name: first parameter
+  * @age: second parameter
+  * @owner: third parameter
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
