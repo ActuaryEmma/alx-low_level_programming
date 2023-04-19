a78fce91a4b74483141d8ad37d83c3cca28921fe
ActuaryEmma <emmanyakio@gmail.com>
2023-04-19 16:55:45 +0300

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned long i;
 
 	/* check if null for size and action pointer */
-	if (array == NULL action == NULL)
+	if (array == NULL || action == NULL)
 	{
 		return;
 	}
