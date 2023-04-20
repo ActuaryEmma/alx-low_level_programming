aaa9b3ece4f7f485a990b10d648dbca51c894469
ActuaryEmma <emmanyakio@gmail.com>
2023-04-21 01:06:01 +0300

    sum of numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
-  * 
+  * sum_them_all - add all numbers
+  * @n: total number of the numbers to be added
+  * Return: sum of numbers
   */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, result = 0;
+
 	va_list(my_list);
+
 	va_start(my_list, n);
 
 	for (i = 0; i < n; i++)
