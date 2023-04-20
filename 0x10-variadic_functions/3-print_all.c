e963988f0544bc9f15b3eb8e239b9149bf4cb915
ActuaryEmma <emmanyakio@gmail.com>
2023-04-21 02:16:09 +0300

    print all :

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+  * print_all - print all formats followed by a separator
+  * @format: list of types of argument
+  */
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	char *var_str;
+	va_list mylist;
+
+	va_start(mylist, formart);
+
+	printf("\n");
+	va_end(mylist);
+}
+
