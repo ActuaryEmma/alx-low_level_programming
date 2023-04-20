1c52964f159807ae48cda92ab8b8a226370bc715
ActuaryEmma <emmanyakio@gmail.com>
2023-04-21 01:46:33 +0300

    print a string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		if (separator != NULL && i < n - 1)
 		{
-			printf("%s\n", separator);
+			printf("%s ", separator);
 		}
 	}
 	printf("\n");
