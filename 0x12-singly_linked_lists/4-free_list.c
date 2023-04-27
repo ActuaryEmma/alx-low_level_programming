14abd41c73e17b0f35480af2fa7694d7e88ec6b7
ActuaryEmma <emmanyakio@gmail.com>
2023-04-27 22:41:42 +0300

    free memory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  * free_list - frees a list_t list
+  * @head: pointer 
+  * Return: void
+  */
+void free_list(list_t *head)
+{
+	/* temp to store next node */
+	list_t *temp;
+
+/* run as long as head is not NULL */
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+
+}
