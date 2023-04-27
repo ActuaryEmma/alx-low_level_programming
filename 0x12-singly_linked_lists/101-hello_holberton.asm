dd72cbb8a7cf6b5b0b8ad6d9ca9e022fd39d3524
ActuaryEmma <emmanyakio@gmail.com>
2023-04-27 23:31:14 +0300

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,10 @@
-/* export the symbol main so that linker can find it*/
 global main
-
-/* declare printf function */
 extern printf
-
-/*entry point of the program */
 main:
 mov edi, format
 xor eax, eax
 call printf
 mov eax, 0
 ret
-
 format: db `Hello, Holberton\n`,0
 
