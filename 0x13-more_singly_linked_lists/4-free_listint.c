95c8d1200ba8b67260ac3a554fb604e904bfbf9e
ActuaryEmma <emmanyakio@gmail.com>
2023-04-28 13:35:26 +0300

    free memory

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+  * free_listint - free the memory
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
