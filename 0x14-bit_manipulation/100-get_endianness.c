096b9af4683d7a9780c790f252a36288f768a4e6
ActuaryEmma <emmanyakio@gmail.com>
2023-05-04 21:17:17 +0300

    advanced tasks

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,7 +2,7 @@
 
 /**
  * get_endianness - function that checks the endianness.
- * checks the endianness of the machine it is running on. 
+ * checks the endianness of the machine it is running on.
  * Endianness refers to the order in which bytes are stored in memory.
  * Return: 0 if big endian, 1 if little endian
  */
