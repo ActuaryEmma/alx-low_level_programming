33745a68b9d839912183c9f5c41f69c98b9c8217
ActuaryEmma <emmanyakio@gmail.com>
2023-05-08 15:28:45 +0300

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file = fopen(filename, "a");
+	file = fopen(filename, "w+");
 	if (file == NULL)
 	{
 		return (-1);
