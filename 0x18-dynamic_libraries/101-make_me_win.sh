97d97b7cc9e6c35538cd017360f6abc75a943f28
ActuaryEmma <emmanyakio@gmail.com>
2023-06-20 12:39:13 +0300

    Task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget https://github.com/ActuaryEmma/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libtest.so -P /tmp
-LD_PRELOAD=/tmp/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
+
